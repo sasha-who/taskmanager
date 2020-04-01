8455d6dbc225ee204f806c07772b9a0bb9dff292
Sasha <warmsasha@gmail.com>
2020-04-01 22:53:37 +0400

    Добавляет функцию render и отрисовывает компоненты на странице

--- src/main.js
+++ src/main.js
@@ -368,3 +368,26 @@ const createLoadMoreButtonTemplate = () => {
     `<button class="load-more" type="button">load more</button>`
   );
 };
+
+const render = (template, container, position) => {
+  container.insertAdjacentHTML(position, template);
+};
+
+const mainElement = document.querySelector(`.main`);
+const mainControlElement = mainElement.querySelector(`.main__control`);
+
+render(createMenuTemplate(), mainControlElement, `beforeend`);
+render(createFilterTemplate(), mainElement, `beforeend`);
+render(createBoardTemplate(), mainElement, `beforeend`);
+
+const boardTasksElement = mainElement.querySelector(`.board__tasks`);
+
+render(createEditableTaskTemplate(), boardTasksElement, `beforeend`);
+
+for (let i = 0; i < TASKS_COUNT; i++) {
+  render(createTaskTemplate(), boardTasksElement, `beforeend`);
+}
+
+const boardElement = mainElement.querySelector(`.board`);
+
+render(createLoadMoreButtonTemplate(), boardElement, `beforeend`);

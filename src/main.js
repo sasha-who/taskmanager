43ece8763943deaeda03bf3ede45d02846bc4a31
Sasha <warmsasha@gmail.com>
2020-04-06 01:17:19 +0400

    Импортирует модули в main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-'use strict';
+import {createMenuTemplate} from "./components/menu.js";
+import {createFilterTemplate} from "./components/filter.js";
+import {createBoardTemplate} from "./components/board.js";
+import {createEditableTaskTemplate} from "./components/task-edit.js";
+import {createTaskTemplate} from "./components/task.js";
+import {createLoadMoreButtonTemplate} from "./components/load-more-button.js";
 
 const TASKS_COUNT = 3;
-
-const createLoadMoreButtonTemplate = () => {
-  return (
-    `<button class="load-more" type="button">load more</button>`
-  );
-};
 
 const render = (template, container, position) => {
   container.insertAdjacentHTML(position, template);

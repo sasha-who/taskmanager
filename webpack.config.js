04c0fa8955faedf727a4d7453aa499a3396578ae
Sasha <warmsasha@gmail.com>
2020-04-05 16:24:28 +0400

    Описывает конфигурацию сборки

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`,
+  entry: `./src/main.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(_dirname, `public`)
+  },
+  devtool: `source-map`
+};

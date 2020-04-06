34d7263ae8be4373b29f868100d6ab0e8dca2650
Sasha <warmsasha@gmail.com>
2020-04-06 22:24:14 +0400

    Меняет в компонентах синтаксис стрелочных функций на сокращённый

--- src/components/load-more-button.js
+++ src/components/load-more-button.js
@@ -1,5 +1,3 @@
-export const createLoadMoreButtonTemplate = () => {
-  return (
-    `<button class="load-more" type="button">load more</button>`
-  );
-};
+export const createLoadMoreButtonTemplate = () => (
+  `<button class="load-more" type="button">load more</button>`
+);

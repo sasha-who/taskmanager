eb6496f6e354e2b4fe90c3ba226616fe69c7e3e4
sasha-who <warmsasha@gmail.com>
2020-04-23 09:52:02 +0300

    Добавляет функцию для создания DOM-элементов

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
 import {CRITICAL_NUMBER} from "./const.js";
 
-const getRandomIntegerNumber = (min, max) => {
+export const getRandomIntegerNumber = (min, max) => {
   return min + Math.floor(Math.random() * (max - min));
 };
 
-const getRandomArrayItem = (elements) => {
+export const getRandomArrayItem = (elements) => {
   const randomIndex = getRandomIntegerNumber(0, elements.length);
 
   return elements[randomIndex];
@@ -14,11 +14,17 @@ const castomizeTimeFormat = (value) => {
   return value < CRITICAL_NUMBER ? `0${value}` : value.toString();
 };
 
-const formatTime = (date) => {
+export const formatTime = (date) => {
   const hours = castomizeTimeFormat(date.getHours() % 12);
   const minutes = castomizeTimeFormat(date.getMinutes());
 
   return `${hours}:${minutes}`;
 };
 
-export {getRandomIntegerNumber, getRandomArrayItem, formatTime};
+export const createElement = (template) => {
+  const container = document.createElement(`div`);
+
+  container.innerHTML = template;
+
+  return container.firstChild;
+};
